caccd07d5d5fedbff8f1bb8845045d3f820e33f8
Alex Vondrak <ajvondrak@gmail.com>
2021-02-04 00:48:46 -0800

    Disable log colors in the debug integration test
    For whatever reason, going through the integration/tests.exs script
    disables ANSI colors, so these tests weren't failing via `mix
    integrate`. However, they'd still fail if I cd'd into the directory and
    manually ran `mix test`. This config just shuts off colors altogether,
    since we make fairly strong assertions about the log output.

--- integration/tests/debug/config/config.exs
+++ integration/tests/debug/config/config.exs
@@ -1,4 +1,7 @@
 use Mix.Config # for compatibility with Elixir >= 1.7 but < 1.9
 
-config :logger, :console, format: "[$level] $message\n"
+config :logger, :console,
+  colors: [enabled: false],
+  format: "[$level] $message\n"
+
 config :remote_ip, debug: true
