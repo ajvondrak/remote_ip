1cbe2a88e384aa851d6077e9c275dcdeb220d08f
Alex Vondrak <ajvondrak@gmail.com>
2021-02-19 14:57:25 -0800

    Print summary after integration tests
    It's easy to lose your place in the output of the integration tests,
    since there are a lot of messages about the dependencies being
    fetched/compiled (which we don't necessarily want to silence). Rather
    than having to `echo $?` every time I run `mix integrate`, I decided to
    add a helpful summary.
    
    Note that the use of the `with` statement now guarantees that we'll stop
    in the middle of the mix commands if one of them fails. Not only is this
    safer than before, it guarantees we only ever append one flag per app to
    the summary.

--- integration/tests.exs
+++ integration/tests.exs
@@ -8,14 +8,21 @@ defmodule Integration.Tests do
   end
 
   def run do
+    {:ok, _} = Agent.start_link(fn -> [] end, name: :summary)
     File.ls!(@path) |> Enum.each(&run/1)
+    summarize()
   end
 
   def run(app) do
     IO.puts("---> Running integration tests on #{app} app")
-    mix(app, "deps.clean", ["--build", "remote_ip"])
-    mix(app, "deps.get")
-    mix(app, "test", [@color])
+
+    with :ok <- mix(app, "deps.clean", ["--build", "remote_ip"]),
+         :ok <- mix(app, "deps.get"),
+         :ok <- mix(app, "test", [@color]) do
+      track(app, :pass)
+    else
+      :error -> track(app, :fail)
+    end
   end
 
   def mix(app, task, args \\ []) do
@@ -26,9 +33,52 @@ defmodule Integration.Tests do
     IO.puts(["-->", "mix" | cmd] |> Enum.join(" "))
     {_, status} = System.cmd("mix", cmd, cd: dir, into: out)
 
-    if status != 0 do
-      System.at_exit(fn _ -> exit({:shutdown, 1}) end)
+    if status == 0 do
+      :ok
+    else
+      :error
     end
+  end
+
+  def track(app, flag) do
+    Agent.update(:summary, fn flags -> flags ++ [{app, flag}] end)
+  end
+
+  def summarize do
+    Agent.get(:summary, fn summary ->
+      counts(summary)
+
+      Enum.each(summary, fn
+        {app, :pass} -> pass(app)
+        {app, :fail} -> fail(app)
+      end)
+    end)
+  end
+
+  def counts(summary) do
+    tests = length(summary)
+    fails = Enum.count(summary, fn {_, flag} -> flag == :fail end)
+    msg = [plural(tests, "integration test"), ", ", plural(fails, "failure")]
+
+    IO.puts("")
+
+    if fails > 0 do
+      IO.ANSI.format([:red | msg]) |> IO.puts()
+    else
+      IO.ANSI.format([:green | msg]) |> IO.puts()
+    end
+  end
+
+  def plural(1, string), do: "1 #{string}"
+  def plural(n, string), do: "#{n} #{string}s"
+
+  def pass(app) do
+    IO.ANSI.format([:green, "  ✓ #{app}"]) |> IO.puts()
+  end
+
+  def fail(app) do
+    IO.ANSI.format([:red, "  ✗ #{app}"]) |> IO.puts()
+    System.at_exit(fn _ -> exit({:shutdown, 1}) end)
   end
 end
 
