cb5e63b8e658418709d4b5b1c7232beb9c9f3517
Alex Vondrak <ajvondrak@gmail.com>
2021-02-27 13:15:54 -0800

    Add RemoteIp.Block
    This module was written to replace the inet_cidr dependency. The main
    reasons I don't like inet_cidr being:
    
    1. It doesn't parse single IPs, so you always have to use the full /32
       or /128 syntax. I'd prefer to let users not worry about this. But
       there's no way to hack around the behavior that isn't clumsy.
    
    2. It doesn't like CIDRs where the lower bits of the IP are lopped off.
       This makes some amount of sense, because it may be hard to realize
       that this is what you're doing. But despite its protestations, even
       these cases are actually valid CIDR notation. We could pass the
       `adjust` flag to InetCidr.parse/2, but other issues sway my decision.
    
    3. Its error messages aren't helpful. It doesn't catch even simple
       cases. For example, an invalid IP address generates a MatchError
       because it doesn't handle the {:error, :einval} case from
       :inet.parse_address/1.
    
    4. Its implementation is not as efficient as it could be. It does all
       its membership checks using integer comparisons across tuple
       elements, versus using the bit manipulation that IPs were designed
       for in in the first place. What's better: doing comparisons across
       four 8-bit integers (and that's assuming the integers aren't just
       generically allocated at 32 bits apiece) or a bit mask across one
       32-bit integer? At least it's not using string matching, I guess...
    
    Other libraries don't fare that well.
    
    * :cidr recognizes bare IP addresses and has slightly better error
      messages, but the CIDR.match!/2 interface is obnoxious. For example,
      it raises errors when checking if IPv4 addresses are contained in an
      IPv6 block, instead of just returning false.
    
    * :erl_cidr is based on :inet_cidr. It might even be using the same
      code, I'm not sure. Point is, it has all the same issues.
    
    * :cider is much better than the others. Its implementation uses bitwise
      operations on integers, it recognizes a variety of syntax, and it has
      some fancy operations to (ostensibly) reduce a list of ranges into a
      minimal set. But I find its interface kind of unwieldy, and it still
      doesn't generate great error messages.
    
    It turns out that rolling my own code isn't so prohibitively complicated
    as to warrant a library. Because of course the point of IPs was always
    to be easy: a couple bitwise operators and Bob's your uncle. Parsing
    takes a little more elbow grease, but I can finally get decent error
    messages out of the deal.
    
    Replacing inet_cidr is probably going to be a bit of an ordeal, so I'm
    committing the RemoteIp.Block code unused before gutting the dependency.

---
+++ lib/remote_ip/block.ex
@@ -0,0 +1,88 @@
+defmodule RemoteIp.Block do
+  use Bitwise
+  alias __MODULE__
+
+  @moduledoc false
+
+  defstruct [:ip, :mask]
+
+  def contains?(%Block{} = block, ip) when is_tuple(ip) do
+    contains?(block, encode(ip))
+  end
+
+  def contains?(%Block{ip: <<sup::32>>, mask: <<mask::32>>}, <<sub::32>>) do
+    (sup &&& mask) == (sub &&& mask)
+  end
+
+  def contains?(%Block{ip: <<sup::128>>, mask: <<mask::128>>}, <<sub::128>>) do
+    (sup &&& mask) == (sub &&& mask)
+  end
+
+  def contains?(%Block{}, _) do
+    false
+  end
+
+  def parse!(cidr) do
+    case parse(cidr) do
+      {:ok, block} -> block
+      {:error, message} -> raise ArgumentError, message
+    end
+  end
+
+  def parse(cidr) do
+    case process(:parts, String.split(cidr, "/", parts: 2)) do
+      {:error, e} -> {:error, "#{e} in CIDR #{inspect(cidr)}"}
+      ok -> ok
+    end
+  end
+
+  defp process(:parts, [ip, prefix]) do
+    with {:ok, ip} <- process(:ip, ip),
+         {:ok, prefix} <- process(:prefix, prefix) do
+      process(:block, ip, prefix)
+    end
+  end
+
+  defp process(:parts, [ip]) do
+    with {:ok, ip} <- process(:ip, ip) do
+      process(:block, ip, bit_size(ip))
+    end
+  end
+
+  defp process(:ip, address) do
+    case :inet.parse_strict_address(address |> to_charlist()) do
+      {:ok, ip} -> {:ok, encode(ip)}
+      {:error, _} -> {:error, "Invalid address #{inspect(address)}"}
+    end
+  end
+
+  defp process(:prefix, prefix) do
+    try do
+      {:ok, String.to_integer(prefix)}
+    rescue
+      ArgumentError -> {:error, "Invalid prefix #{inspect(prefix)}"}
+    end
+  end
+
+  defp process(:block, <<_::32>> = ip, prefix) when prefix in 0..32 do
+    ones = 0xFFFFFFFF
+    {:ok, %Block{ip: ip, mask: <<(~~~(ones >>> prefix))::32>>}}
+  end
+
+  defp process(:block, <<_::128>> = ip, prefix) when prefix in 0..128 do
+    ones = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+    {:ok, %Block{ip: ip, mask: <<(~~~(ones >>> prefix))::128>>}}
+  end
+
+  defp process(:block, _, prefix) do
+    {:error, "Invalid prefix #{inspect(prefix)}"}
+  end
+
+  defp encode({a, b, c, d}) do
+    <<a::8, b::8, c::8, d::8>>
+  end
+
+  defp encode({a, b, c, d, e, f, g, h}) do
+    <<a::16, b::16, c::16, d::16, e::16, f::16, g::16, h::16>>
+  end
+end
