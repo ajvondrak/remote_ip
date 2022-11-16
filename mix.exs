0d253fc0ad8f674396b94f72e9d2fc0792b3e007
Alex Vondrak <ajvondrak@gmail.com>
2022-11-16 13:26:11 -0800

    Prepare v1.1 release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RemoteIp.Mixfile do
   def project do
     [
       app: :remote_ip,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.10",
       description: description(),
       package: package(),
