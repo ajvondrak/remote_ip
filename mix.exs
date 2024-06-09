fbb6b67a29072cdd7083aa2f27fe5e953d211a43
Alex Vondrak <ajvondrak@gmail.com>
2024-06-09 16:48:21 -0700

    Add castore dependency for CI
    ** (ExCoveralls.ReportUploadError) Failed to use any SSL certificates.
    We didn't find the :castore library, and we couldn't use OS certificates
    because that requires OTP 25 or later. If you want to use :castore,
    please add
    
      {:castore, "~> 1.0"}

--- mix.exs
+++ mix.exs
@@ -39,7 +39,8 @@ defmodule RemoteIp.Mixfile do
       {:plug, "~> 1.14"},
       {:ex_doc, "~> 0.34", only: :dev, runtime: false},
       {:dialyxir, "~> 1.4", only: [:ci, :dev], runtime: false},
-      {:excoveralls, "~> 0.18", only: [:ci, :test], runtime: false}
+      {:excoveralls, "~> 0.18", only: [:ci, :test], runtime: false},
+      {:castore, "~> 1.0", only: [:ci, :test], runtime: false}
     ]
   end
 
